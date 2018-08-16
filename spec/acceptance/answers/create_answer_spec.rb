923837435be630665fa7e5af2613959bc5893b4d
insrf <ins_rf@mail.ru>
2018-08-16 22:47:51 +0500

    add js: true to acceptance answer create test

--- spec/acceptance/answers/create_answer_spec.rb
+++ spec/acceptance/answers/create_answer_spec.rb
@@ -7,8 +7,7 @@ feature 'User can fill form for answer on questions page', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:question) { create(:question) }
-  given(:invalid_answer) { create(:invalid_answer, question_id: question.id) }
+  given!(:question) { create(:question) }
 
   scenario 'Non-authenticated user try to fill form to answer on question page' do
       visit question_path(question)
@@ -18,15 +17,18 @@ feature 'User can fill form for answer on questions page', %q{
       expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 
-  scenario 'Authenticated user try to fill form to answer on question page' do
+  scenario 'Authenticated user try to fill form to answer on question page', js: true do
       sign_in(user)
 
       visit question_path(question)
       fill_in 'Body', with: "some answer"
       click_on 'Create Answer'
 
-      expect(page).to have_content 'Your answer successfully created.'
-      expect(page).to have_content 'some answer'
+      expect(current_path).to eq question_path(question)
+      # expect(page).to have_content 'Your answer successfully created.'
+      within '.answers' do
+        expect(page).to have_content 'some answer'
+      end
   end
 
   scenario 'Authenticated user try to fill form to invalid answer on question page' do
